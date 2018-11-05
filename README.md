10907d98f1f846a181c5866b761c5b425df4d3e8
Jakub Dobrowolski <jakub.dobrowolski@besidethepark.com>
2018-11-05 09:44:23 +0100

    git setup

---
+++ README.md
@@ -0,0 +1,29 @@
+# react-webpack-babel-kit
+
+A simple hello world using react, webpack, babel, es2015 & sass. 
+
+Compiles all the scripts & styles from `src` path to `public`. The kit generates source maps by default in development mode.
+
+##Installation 
+
+```bash
+    $ npm install 
+```
+
+##Usage
+
+To build on development mode, generates a minified version with source map.
+
+```bash
+    $ npm run build
+```
+
+
+
+To use webpack dev server & watch,
+
+```bash
+    $ npm start
+```
+
+Listens at **http://localhost:9000** 
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const WebpackCleanupPlugin = require ('webpack-cleanup-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+ 
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }, {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader','postcss-loader']
+        })
+      }, {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+    },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+            {
+                loader: "file-loader",
+                options: {
+                    outputPath: "assets/"
+                }
+            }
+        ]
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({template: "./src/index.html"}),
+    new ExtractTextPlugin({filename: './app.bundle.css'}),
+    new WebpackCleanupPlugin()
+  ]
+};
