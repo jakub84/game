c945ffec929c88dffe8e49b9f1d7628af2238f40
Jakub Dobrowolski <jakub.dobrowolski@besidethepark.com>
2018-11-07 16:11:18 +0100

    Add some logic improvements

--- src/Components/App.js
+++ src/Components/App.js
@@ -42,24 +42,39 @@ class App extends React.Component {
     super(props)
     this.state = {
       UserAnswer: '',
-      placeholder:'Type your answer',
+      placeholder: 'Type your answer',
       dinamicPassword: '',
       isSucces: false,
       isLoose: false,
       counter: 0,
-      compareAnswers:[],
-      category:''
+      compareAnswers: [],
+      category: '',
+      hiddenPassword: []
     }
   }
 
+  looseGame() {
+    if(this.state.counter === 5) {
+      this.setState({
+        isSucces: false,
+        isLoose: true
+      })
+    }
+  }
 
   letChoosePassword = (max) => {
+    this.setState({
+      dinamicPassword: ''
+    })
     let customNr = Math.floor((Math.random() * max))
-    console.log(customNr)
     this.setState({
       dinamicPassword: allPaswords[customNr].password,
-      category: allPaswords[customNr].category
+      category: allPaswords[customNr].category,
+      isSucces: false,
+      isLoose: false,
+      UserAnswer: '',
     })
+    console.log(this.state.hiddenPassword)
   }
 
   onFormSubmit = (e) => {
@@ -68,82 +83,51 @@ class App extends React.Component {
     this.setState({
       // UserAnswer: e.target.value,
       counter: this.state.counter + 1
-      
+
     })
-    // this.looseGame()
+    this.looseGame()
   }
   onChangeInput = (e) => {
     this.setState({
       UserAnswer: e.target.value,
-
     })
 
   }
-  // checkAnswers = (userAnswer, pass) => {
-  //   let newTab = []
-  //   userAnswer = userAnswer.toLowerCase().split('')
-  //   pass = pass.toLowerCase().split('')
-  //   userAnswer.forEach((userAnswers, i) => {
-  //     pass.forEach((passwords, j) => {
-  //       if (userAnswers === passwords && i === j) {
-  //           newTab.push(passwords)
-  //         return
-  //       } else if (userAnswers !== passwords && i === j) {
-  //         newTab.push('*')
-  //       } else if (i > j) {
-  //         return
-  //       }
-  //     })
-  //   })
-  //   this.state.compareAnswers = newTab
-  //   if (this.state.compareAnswers.includes('*') || userAnswer.length !== pass.length) {
-  //     this.setState({
-  //       isSucces: false
-  //     })
-  //   } else {
-  //     this.setState({
-  //       isSucces: true
-  //     })
-  //   }
-
-  //   console.log(this.state.compareAnswers, this.state.isSucces, this.state.counter)
-  // }
-
 
   checkAnswers = (userAnswer, pass) => {
     let newTab = []
     userAnswer = userAnswer.toLowerCase().split('')
     pass = pass.toLowerCase().split('')
     pass.filter((val) => {
-     if (userAnswer.includes(val)) newTab.push(val)
-     else if (!userAnswer.includes(val)) newTab.push('*')  
-  })
+      if (userAnswer.includes(val)) newTab.push(val)
+      else if (!userAnswer.includes(val)) newTab.push('_')
+    })
     this.state.compareAnswers = newTab
-    if (this.state.compareAnswers.includes('*') || userAnswer.length !== pass.length) {
-          this.setState({
-            isSucces: false
-          })
-        } else if (this.state.counter === 5) {
-          this.setState({
-            isLoose: false
-          })
-        } 
-        
-        else {
-          this.setState({
-            isSucces: true
-          })
-        }
-    
-        console.log(this.state.compareAnswers, this.state.isSucces, this.state.counter)
+    if (this.state.compareAnswers.includes('_') || userAnswer.length !== pass.length) {
+      this.setState({
+        isSucces: false
+      })
+    } else if (this.state.counter === 5) {
+      this.setState({
+        isLoose: false
+      })
+    }
+
+    else {
+      this.setState({
+        isSucces: true
+      })
+    }
+
+    console.log(this.state.compareAnswers, this.state.isSucces, this.state.counter)
   }
 
   render() {
-    
+
     return (
-      
+
       <div>
-        
+
         <Header />
         <Score
           isWinner={this.state.isSucces}
@@ -151,20 +135,22 @@ class App extends React.Component {
         />
         <PrintAnswer
           password={this.state.compareAnswers}
-          category = {this.state.category}
-          />
+          // hiddenPassword={this.state.compareAnswers.fill('-')}
+          category={this.state.category}
+          tries = {5 - this.state.counter}
+        />
         <InputField
           onFormSubmit={this.onFormSubmit}
-          placeholder = {this.state.placeholder}
+          placeholder={this.state.placeholder}
           inputValue={this.state.UserAnswer}
           onChangeInput={this.onChangeInput}
           checkAnswers={() => this.checkAnswers(this.state.UserAnswer, this.state.dinamicPassword)}
           maxLength={this.state.dinamicPassword.length}
           isBtnDisabled={this.state.isSucces || this.state.isLoose}
           isInputDisabled={this.state.isSucces || this.state.isLoose || this.state.counter === 0}
-          chooseBtn = {() => this.letChoosePassword(allPaswords.length)}
-          btnValue = {this.state.counter === 0 ? 'start' : 'check'}
-          counter = {this.state.counter}
+          chooseBtn={() => this.letChoosePassword(allPaswords.length)}
+          btnValue={this.state.counter === 0 ? 'start' : 'check'}
+          counter={this.state.counter}
         />
 
         {/* <DrawImage /> */}
--- src/Components/PrintAnswer.js
+++ src/Components/PrintAnswer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 const PrintAnswer = (props) => (
     <div className="print-answer">
-        <p className="pass-info">This word has <span>{props.password.length}</span> letters.</p>
+        {props.password.length > 0 ? <p className="pass-info">This word has <span>{props.password.length}</span> letters.</p>: <p></p>}
         <p className="category">Category:
             <span> {props.category}</span>
         </p>
-        <p className="pass">{props.password}</p>
+        {props.tries < 5 && props.tries >= 0 ? <p>You have {props.tries} tries</p>: <p></p>}
+        {props.password && <p className="pass">{props.password}</p>}
         
     </div>
 );
